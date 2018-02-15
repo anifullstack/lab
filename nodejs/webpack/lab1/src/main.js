9a89857fdb0d60890a454bcad6b4107ff141c307
ani <ani.fullstack@gmail.com>
2018-02-15 02:05:58 +0000

    webpack babel initial setup

--- nodejs/webpack/lab1/src/main.js
+++ nodejs/webpack/lab1/src/main.js
@@ -1,5 +1,8 @@
 require('./main.css');
 require('./index.html');
-console.log("main.js v0.0.20");
+console.log("main.js v0.0.21");
 
 
+var sum = (a,b) => a+b;
+
+console.log("main", "sum", sum(4,12));
