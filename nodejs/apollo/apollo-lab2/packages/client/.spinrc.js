d2a361f0a8369d1f0c9a88e4c683d1e94fef1614
ani <ani.fullstack@gmail.com>
2018-02-16 19:02:26 +0000

    appollo starter kit. fixed problems wrt running on cloud ide

--- nodejs/apollo/apollo-lab2/packages/client/.spinrc.js
+++ nodejs/apollo/apollo-lab2/packages/client/.spinrc.js
@@ -15,7 +15,8 @@ const config = {
       webpackConfig: {
         devServer: {
          host: "0.0.0.0",
-         port: 3000
+         port: 3000,
+         disableHostCheck: true
       }
      }
     },
